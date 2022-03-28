bf29d0a7067003d9ea931596855bdbe87f2175d6
David <davidpvh2020@gmail.com>
2022-03-27 20:46:21 -0400

    adicionando verbo PUT para atualizar dados

--- api/rotas/fornecedores/tabelaFornecedor.js
+++ api/rotas/fornecedores/tabelaFornecedor.js
@@ -19,5 +19,14 @@ module.exports = {
         }
 
         return encontrado
+    },
+    atualizar (id, dadosParaAtualizar) {
+        return Modelo.update(
+            dadosParaAtualizar,
+            {
+                where: { id: id }
+            }
+        )
     }
+
 }
