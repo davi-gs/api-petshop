5d6de86c2258555962c236b28a4933b26fe2e1d7
David <davidpvh2020@gmail.com>
2022-03-30 22:43:07 -0400

    adicionando serializador fornecedores e filtrando dados

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,13 +1,17 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./tabelaFornecedor')
 const Fornecedor = require('./fornecedor')
+const SerializadorFornecedor = require('../../Serializador').SerializadorFornecedor
 
 
 roteador.get('/', async (requisicao, resposta) => {
     const resultados = await TabelaFornecedor.listar()
     resposta.status(200)
+    const serializador = new SerializadorFornecedor(
+        resposta.getHeader('Content-Type')
+    )
     resposta.send(
-        JSON.stringify(resultados)
+        serializador.serializar(resultados)
     )
 })
 
@@ -17,8 +21,11 @@ roteador.post('/', async (requisicao, resposta, proximo) => {
         const fornecedor = new Fornecedor(dadosRecebidos)
         await fornecedor.criar()
         resposta.status(201)
+        const serializador = new SerializadorFornecedor(
+            resposta.getHeader('Content-Type')
+        )
         resposta.send(
-            JSON.stringify(fornecedor)
+            serializador.serializar(fornecedor)
         )
     } catch (erro) {
         proximo(erro)
@@ -32,8 +39,11 @@ roteador.get('/:idFornecedor', async (requisicao, resposta, proximo) => {
         const fornecedor = new Fornecedor ({ id: id})
         await fornecedor.carregar()
         resposta.status(200)
+        const serializador = new SerializadorFornecedor(
+            resposta.getHeader('Content-Type')
+        )
         resposta.send(
-            JSON.stringify(fornecedor)
+            serializador.serializar(fornecedor)
         )
     } catch (erro) {
        proximo(erro)
